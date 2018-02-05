6f5cee1bc720012936bcd859c794dad6389ee98e
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2018-02-05 21:50:16 +0200

    basic word fall game

---
+++ Assets/Scripts/ScreenAreas.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenAreas : MonoBehaviour {
+
+    public BoxCollider2D[] allScreens;
+    public BoxCollider2D[] topScreens;
+    public BoxCollider2D mainScreen;
+
+}
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility {
+
+    static System.Random prng;
+
+    static Utility()
+    {
+        prng = new System.Random();
+    }
+
+    public static void Shuffle<T>(T[] array)
+	{
+		for (int i = array.Length; i > 1; i--)
+		{
+			// Pick random element to swap.
+            int j = prng.Next(i); // 0 <= j <= i-1
+									// Swap.
+			T tmp = array[j];
+			array[j] = array[i - 1];
+			array[i - 1] = tmp;
+		}
+	}
+}
