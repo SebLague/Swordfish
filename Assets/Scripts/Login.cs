02380deacd6a1576c1a5d64e1073e56592ac8acc
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2018-02-06 14:08:39 +0200

    login screen

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour {
+
+    public InputField loginField;
+    public Text attemptsRemainingUI;
+
+    bool loginNeedsFocus;
+    int attemptsRemaining = 3;
+
+    string actualPassword = "test";
+	
+	void Start () {
+        attemptsRemainingUI.text = "Attempts remaining: " + attemptsRemaining;
+        loginField.onEndEdit.AddListener(OnEndEdit);
+        ShowLoginPage();
+	}
+	
+	void LateUpdate () {
+        if (loginNeedsFocus)
+        {
+            loginNeedsFocus = false;
+			loginField.Select();
+            loginField.ActivateInputField();
+            loginField.ForceLabelUpdate();
+		}
+	}
+
+    void ShowLoginPage()
+    {
+		loginField.Select();
+
+    }
+
+    void OnEndEdit(string s)
+    {
+        SubmitInput(s);
+        loginField.text = "";
+        loginNeedsFocus = true;
+    }
+
+    void SubmitInput(string enteredPassword)
+    {
+        if (actualPassword.ToLower() == enteredPassword.ToLower())
+        {
+            OnCorrectPassword();
+        }
+        else
+        {
+            OnIncorrectPassword();
+        }
+    }
+
+    void OnCorrectPassword()
+    {
+		print("Win");
+    }
+
+    void OnIncorrectPassword()
+    {
+        attemptsRemaining--;
+        attemptsRemainingUI.text = "Attempts remaining: " + attemptsRemaining;
+
+        if (attemptsRemaining <= 0)
+        {
+            print("Game over");
+        }
+    }
+}
